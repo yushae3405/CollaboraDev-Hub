10507cf8982714c0756a5d2f226987ee986fc34d
Yushae <yushae3405@gmail.com>
2023-12-12 03:41:49 +0530

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app  = express();
-app.get('/',(req,res)=>res.send('API Running'));
 const PORT  = process.env.PORT  || 5000;
+app.get('/',(req,res)=>res.send('API Running'));
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`));
 
